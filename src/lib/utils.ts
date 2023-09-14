7d6e6b36b8dcd5013f970754020fc98d8629cf3d
Domin-MND <69919939+domin-mnd@users.noreply.github.com>
2023-09-14 18:32:59 +0300

    fix: hex as palette initial arg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import type { HEX, RGB, HSL, CMYK } from "@cvet/types";
+import type { HEX, RGB, HSL, CMYK, RGBA } from "@cvet/types";
 import { Hexadecimal } from "../../types";
 
 /**
@@ -34,15 +34,16 @@ export function hueToRgb(p: number, q: number, t: number): number {
 }
 
 /**
- * Convert HEX color to RGB.
- * @param {HEX} hex An HEX color used to convert to RGB.
+ * Convert HEX color to RGB or RGBA.
+ * @param {HEX} hex An HEX color used to convert to RGB or RGBA.
  * @returns {RGB} An RGB color.
  */
-export function hexToRgb(hex: HEX): RGB {
+export function hexToRgb(hex: HEX): RGB | RGBA {
   const r = parseInt(hex.slice(1, 3), 16) as Hexadecimal;
   const g = parseInt(hex.slice(3, 5), 16) as Hexadecimal;
   const b = parseInt(hex.slice(5, 7), 16) as Hexadecimal;
-  return { r, g, b };
+  const a = hex.slice(7, 9) ? parseInt(hex.slice(7, 9), 16) / 2.55 : null;
+  return { r, g, b, ...(a !== null && { a }) };
 }
 
 /**
--- tests/palette.test.ts
+++ tests/palette.test.ts
@@ -19,11 +19,34 @@ test("Color conversion", () => {
   expect(palette.cmyk).toStrictEqual({ c: 0, m: 100, y: 100, k: 0 });
 });
 
+test("Alpha color conversion (HEX)", () => {
+  const palette = new Palette("#FF000033", "HEX");
+
+  expect(palette.hex).toBe("#ff000033");
+  expect(palette.rgb).toStrictEqual({ r: 255, g: 0, b: 0 });
+  expect(palette.rgba).toStrictEqual({ r: 255, g: 0, b: 0, a: 20 });
+  expect(palette.hsl).toStrictEqual({ h: 0, s: 100, l: 50 });
+  expect(palette.hsla).toStrictEqual({ h: 0, s: 100, l: 50, a: 20 });
+  expect(palette.cmyk).toStrictEqual({ c: 0, m: 100, y: 100, k: 0 });
+});
+
+test("Alpha color conversion (HSLA)", () => {
+  const palette = new Palette({ h: 0, s: 100, l: 50, a: 20 }, "HSLA");
+
+  expect(palette.hex).toBe("#ff000033");
+  expect(palette.rgb).toStrictEqual({ r: 255, g: 0, b: 0 });
+  expect(palette.rgba).toStrictEqual({ r: 255, g: 0, b: 0, a: 20 });
+  expect(palette.hsl).toStrictEqual({ h: 0, s: 100, l: 50 });
+  expect(palette.hsla).toStrictEqual({ h: 0, s: 100, l: 50, a: 20 });
+  expect(palette.cmyk).toStrictEqual({ c: 0, m: 100, y: 100, k: 0 });
+});
+
 test("Getters", () => {
   const palette = new Palette("#FF0000", "HEX");
 
   expect(palette.red).toBe(255);
   expect(palette.green).toBe(0);
   expect(palette.blue).toBe(0);
+  expect(palette.alpha).toBe(null);
   expect(palette.color).toStrictEqual({ r: 255, g: 0, b: 0 });
 });
